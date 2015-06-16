c57558fc6f4dd32865f132d5fa29986ce6d14c73
Josh Tomlinson <jtomlin@clemson.edu>
2015-06-16 10:34:43 -0400

    First pass at maya maps import

--- dpa/maya/entity/maps.py
+++ dpa/maya/entity/maps.py
@@ -1,8 +1,9 @@
 
-from dpa.app.entity import Entity, EntityError, EntityRegistry
+from dpa.app.entity import EntityError, EntityRegistry
+from dpa.maya.entity.base import MayaEntity
 
 # -----------------------------------------------------------------------------
-class MapsEntity(Entity):
+class MapsEntity(MayaEntity):
 
     category = 'maps'
 
@@ -12,63 +13,133 @@ class MapsEntity(Entity):
         **kwargs):
         """Import maps into the session."""
 
+        if not session.cmds.pluginInfo('RenderMan_for_Maya', query=True,
+            loaded=True):
+
+            # load the plugin
+            session.cmds.loadPlugin('RenderMan_for_Maya')
+
         product_version = representation.product_version
         product = product_version.product
 
         product_name_parts = product.name.split("_")
         maps_type = product_name_parts.pop()
 
-        # create renderman file node
-        shader_type = 'RMSGPSurface'
-        shader_name = "shader_{pn}_".format(pn=product.name)
-        session.cmds.shadingNode(shader_type, asShader=True, name=shader_name)
+        # ---- file read node
 
-        # add renderman attributes
-        add_attr = 'rmanAddAttr {sn} {attr} "";')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__tx2dFilter')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__tx2dSwidth')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__tx2dTwidth')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__tx2dLerp')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__applysRGB')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__udim')
-        session.mel.eval(add_attr.format(sn=shader_name, attr='rman__unpremultiply')
-
-        # type specific attributes
-        if map_type in ['diff', 'diffuse', 'diffMap', 'diffuseMap']:
-            # XXX
-        elif map_type in ['spec', 'specular', 'specMap', 'specularMap']:
-            # XXX
-        elif map_type in ['bump', 'bumpMap']:
-            # XXX
-        elif map_type in ['disp', 'displacement', 'dispMap', 'displacementMap']:
-            # XXX
-        elif map_type in ['trans', 'transparency', 'transMap', 'transparencyMap']:
-            # XXX
-        else:
-            raise EntityError("Don't know how to process map type: " + map_type)
-
-        # set attr syntax
-        set_attr = '{sn}.{attr}'
-
-        # linearize?
-        session.cmds.setAttr(
-            set_attr.format(sn=shader_name, attr='rman__applysRGB'),
-            kwargs.get('linearize', False)
-        )
-
-        # XXX config options:
-        disable_file_load = kwargs.get('disable_file_load', False)
-
-        # ensure filter type off
-        session.cmds.setAttr(
-            set_attr.format(sn=shader_name, attr='rman__tx2dFilter'), # XXX
-            False, 
-        )
+        file_node_name = "file_{pn}".format(pn=product.name)
+        file_node = session.cmds.shadingNode('file', asTexture=True,
+            name=file_node_name)
 
         # set UDIM/ATLAS support to mari
-        # set image path to: <import_dir>/file_base.__MAPID__.<ext>
+        import_base = cls.get_import_file_common_base(session, 
+            product.name, product.category, representation)
+        map_path = import_base + '._MAPID_.' + representation.type
+        session.cmds.setAttr(file_node + '.fileTextureName', map_path,
+            type="string")
+        session.cmds.setAttr(file_node + '.filterType', 0)
+        session.cmds.setAttr(file_node + '.colorProfile', 2) 
 
-        # hook file nodes to shader
+        # add renderman attributes
+        add_attr = 'rmanAddAttr {fn} {attr} "";'
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__tx2dFilter'))
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__tx2dSwidth'))
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__tx2dTwidth'))
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__tx2dLerp'))
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__applysRGB'))
+        session.mel.eval(add_attr.format(fn=file_node, attr='rman__udim'))
+        session.mel.eval(add_attr.format(fn=file_node,
+            attr='rman__unpremultiply'))
+
+        # set udim lookup to 'mari'
+        session.cmds.setAttr(file_node + '.rman__udim', 'mari', type='string')
+
+        # ---- config options
+
+        # linearize?
+        session.cmds.setAttr(file_node + '.rman__applysRGB',
+            kwargs.get('linearize', False))
+
+        # disable file load 
+        session.cmds.setAttr(file_node + '.disableFileLoad',
+            kwargs.get('disable_file_load', False))
+
+        # ---- create surface shader 
+
+        shader_type = 'RMSGPSurface'
+        shader_name = "shader_{pn}".format(pn=product.name)
+        session.cmds.shadingNode(shader_type, asShader=True, name=shader_name)
+
+        # ---- type specific attributes and connections
+
+        connect = kwargs.get('connect', True)
+
+        # DIFFUSE
+        if maps_type in ['diff', 'diffuse', 'diffMap', 'diffuseMap']:
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor',
+                    shader_name + '.surfaceColor')
+
+        # SPECULAR
+        elif maps_type in ['spec', 'specular', 'specMap', 'specularMap']:
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor',
+                    shader_name + '.specularColor')
+
+        # BUMP
+        elif maps_type in ['bump', 'bumpMap']:
+
+            session.cmds.setAttr(file_node + '.alphaIsLuminance', 1)
+
+            session.mel.eval('rmanSetAttr {sn} {attr} 0'.format(
+                sn=shader_name, attr='enableDisplacement'))
+
+            session.cmds.setAttr(shader_name + '.bumpAmount', 1.0)
+
+            if connect:
+                session.cmds.connectAttr( file_node + '.outColor.outColorR', 
+                    shader_name + '.bumpScalar')
+
+        # TRANSPARENCY
+        elif maps_type in ['trans', 'transparency', 'transMap',
+            'transparencyMap']:
+
+            session.cmds.setAttr(file_node + '.alphaIsLuminance', 1)
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor',
+                    shader_name + '.transparency')
+
+        # DISPLACEMENT
+        elif maps_type in ['disp', 'displacement', 'dispMap', 'displacementMap']:
+
+            add_attr = 'rmanAddAttr {sn} {attr} "";'
+            session.mel.eval(add_attr.format(sn=shader_name, 
+                    attr='rman__riattr__displacementbound_sphere'))
+
+            session.mel.eval(add_attr.format(sn=shader_name,
+                    attr='rman__riattr__displacementbound_coordinatesystem'))
+
+            session.mel.eval(add_attr.format(sn=shader_name,
+                    attr='rman__riattr__trace_displacements'))
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor.outColorR',
+                    shader_name + '.displacementScalar')
+
+            session.cmds.setAttr(shader_name + '.displacementAmount', 1.0)
+
+            session.cmds.setAttr(
+                shader_name + ".rman__riattr__displacementbound_sphere", 1.0)
+
+            session.mel.eval('rmanSetAttr {sn} {val} 0;'.format(sn=shader_name,
+                    val='enableDisplacement'))
+
+        # UNKNOWN
+        else:
+            raise EntityError("Don't know how to process map type: " + maps_type)
 
     # -------------------------------------------------------------------------
     def export(self, *args, **kwargs):
