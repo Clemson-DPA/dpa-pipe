a6715fc7130a516de24e0f203bcc3a74908ed972
Alexander Doug Rizeakos <arizeak@l0028.fx.clemson.edu>
2015-11-10 14:54:51 -0500

    changed the maps import and config file to support arnold shader

--- dpa/maya/entity/maps.py
+++ dpa/maya/entity/maps.py
@@ -11,6 +11,16 @@ class MapsEntity(MayaEntity):
     @classmethod
     def import_product_representation(cls, session, representation, *args, 
         **kwargs):
+        shader_name = kwargs.get('shader', False)[0]
+        if( shader_name == 'RMSGPSurface' ):
+            MapsEntity.rendermanImport(session, representation, kwargs)
+        elif( shader_name == 'aiStandard' ):
+            MapsEntity.arnoldImport(session, representation, kwargs)
+
+    @classmethod
+    def rendermanImport(cls, session, representation, kwargs):
+
+
         """Import maps into the session."""
 
         session.require_plugin('RenderMan_for_Maya')
@@ -144,6 +154,85 @@ class MapsEntity(MayaEntity):
         else:
             raise EntityError("Don't know how to process map type: " + maps_type)
 
+    @classmethod
+    def arnoldImport(cls, session, representation, kwargs):
+        print "arnold create"
+
+        session.require_plugin('mtoa')
+
+        ## Same as renderman ##
+        product_version = representation.product_version
+        product = product_version.product
+
+        product_name_parts = product.name.split("_")
+        maps_type = product_name_parts.pop()
+        obj_name = "_".join(product_name_parts)
+
+        # ---- file read node
+
+        file_node_name = "file_{pn}".format(pn=product.name)
+        file_node = session.cmds.shadingNode('file', asTexture=True,
+            name=file_node_name)
+
+        # set UDIM/ATLAS support to mari
+        current_file = session.cmds.file(q=True, sceneName=True)
+        import_base = cls.get_import_file_common_base(session, 
+            product.name, product.category, representation,
+            relative_to=current_file
+        )
+        ## changed to arnold replacement token <udim>
+        map_path = import_base + '.<udim>.' + representation.type
+        session.cmds.setAttr(file_node + '.fileTextureName', map_path,
+            type="string")
+        session.cmds.setAttr(file_node + '.filterType', 0)
+        session.cmds.setAttr(file_node + '.colorProfile', 0) 
+        ##
+
+        # ---- create surface shader if it doesn't exist
+
+        shader_type = 'aiStandard'
+        shader_name = "shader_{on}".format(on=obj_name)
+
+        if not session.cmds.ls(shader_name):
+            session.cmds.shadingNode(shader_type, asShader=True,
+                name=shader_name)
+
+        # ---- type specific attributes and connections
+
+        connect = kwargs.get('connect', True)
+
+        # DIFFUSE
+        if maps_type in ['diff', 'diffuse', 'diffMap', 'diffuseMap']:
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor',
+                    shader_name + '.color')
+
+        # SPECULAR
+        elif maps_type in ['spec', 'specular', 'specMap', 'specularMap']:
+
+            if connect:
+                session.cmds.connectAttr(file_node + '.outColor',
+                    shader_name + '.KsColor')
+
+        # BUMP
+        elif maps_type in ['bump', 'bumpMap']:
+
+            session.cmds.setAttr(file_node + '.alphaIsLuminance', 1)
+
+            bump_node = session.cmds.shadingNode('bump2d', asTexture=True)
+
+            if connect:
+                session.cmds.connectAttr( file_node + '.outAlpha', 
+                    bump_node + '.bumpValue')
+                session.cmds.connectAttr( bump_node + '.outNormal', 
+                    shader_name + '.normalCamera')
+
+        # TRANSPARENCY
+        ## not supported for now
+
+        #temp, sets arnold texture path, should be set on dpaopen most likely
+        session.cmds.setAttr( 'defaultArnoldRenderOptions.texture_searchpath', session.ptask_area.path, type="string" )
     # -------------------------------------------------------------------------
     def export(self, *args, **kwargs):
         raise EntityError("Maya maps export not implemented.") 
